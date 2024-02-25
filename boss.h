dce3e19a2a221f1165bcefbccc62ca71ec7951da
chanon-plu <146055285+chanon-plu@users.noreply.github.com>
2024-02-25 05:28:04 +0000

    add commit

---
+++ boss.h
@@ -0,0 +1,14 @@
+#include <string>
+
+class Boss{
+    private:
+        string name;
+        int health,attack,defense;
+
+    public:
+        Boss(string, int, int);
+};
+
+Boss::Boss(){
+    
+}
